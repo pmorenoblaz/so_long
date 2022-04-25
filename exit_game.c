1b81c99f633d61a636a643078b826a22e0801a82
Paola Moreno <pmoreno-@student.42madrid.com>
2022-04-25 20:36:21 +0200

    :(

--- exit_game.c
+++ exit_game.c
@@ -6,7 +6,7 @@
 /*   By: pmoreno- <pmoreno-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 20:41:49 by pmoreno-          #+#    #+#             */
-/*   Updated: 2022/04/25 20:33:08 by pmoreno-         ###   ########.fr       */
+/*   Updated: 2022/04/25 20:36:05 by pmoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,8 @@ int	key_hook(int keycode, t_data *data)
 	{
 		if ((data->player.y + 1) < (data->cont->y - 1))
 		{
+			write(1, "puedes mover\n", 13);
+			write(1, "puedes mover\n", 13);
 			write(1, "puedes mover\n", 13);
 			data->player.y += 1;
 		}
